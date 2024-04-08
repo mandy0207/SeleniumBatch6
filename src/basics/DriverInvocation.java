6f6479d4035a5d2cd0d1676beccf0a103c13889c
unknown <mandyatwork24@gmail.com>
2024-04-08 14:57:54 +0530

    basic commands

---
+++ src/basics/DriverInvocation.java
@@ -0,0 +1,42 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverInvocation {
+
+	public static void main(String[] args) throws InterruptedException {
+		/**
+		 * ctrl+shift+o- > import packages
+		 */
+	
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.google.com/");
+		
+		//command to grab title 
+		String title=driver.getTitle();
+		System.out.println(title);
+		
+		//command to current URL
+		String url=driver.getCurrentUrl();
+		System.out.println(url);
+		
+		//command to navigate backward
+		driver.navigate().back();
+		
+		//waiting
+		Thread.sleep(2000);
+		
+		//command to navigate forward
+		driver.navigate().forward();
+		
+		//command to quit browser
+		driver.quit();
+		
+		
+	
+	}
+
+}

c118dfa239e2e68987aaa9a409dbb2ad98af7971
unknown <mandyatwork24@gmail.com>
2024-04-11 13:07:55 +0530

    xpath

---
+++ src/basics/LearnXpath.java
@@ -0,0 +1,36 @@
+package basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnXpath {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://demowebshop.tricentis.com/");
+		/**
+		 * 
+		 * Syntax of Xpath :
+		 * 1) //tagname[@atrribute='value'] or //*[@atrribute='value']
+		 * 2) //tagname
+		 * 3)  //tagname[@atrribute='value']/tagname
+		 * 4)  //parent/child[index]
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		driver.findElement(By.xpath("//a[@class='ico-login']")).click();
+		driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("obsqura24@gmail.com");
+		driver.findElement(By.xpath("//input[@name='Password']")).sendKeys("mypassword");
+		driver.findElement(By.xpath("//*[@class='buttons']/input[@value='Log in']")).click();
+		driver.findElement(By.xpath("//ul[@class='list']/li[4]/a")).click();
+
+	}
+
+}

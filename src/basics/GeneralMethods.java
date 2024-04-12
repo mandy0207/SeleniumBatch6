c53ea3f545d55a99db775b4ae5c8daec5f9e5f58
unknown <mandyatwork24@gmail.com>
2024-04-12 12:54:47 +0530

    genera methods

--- src/basics/GeneralMethods.java
+++ src/basics/GeneralMethods.java
@@ -2,6 +2,7 @@ package basics;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GeneralMethods {
@@ -9,11 +10,27 @@ public class GeneralMethods {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		
+
 		driver.get("https://yatra.com/");
-		
-		String headingText=driver.findElement(By.xpath("//*[@class='main-heading']")).getText();
-        System.out.println(headingText);
+
+		// grab text of any webelement
+		String headingText = driver.findElement(By.xpath("//*[@class='main-heading']")).getText();
+		System.out.println(headingText);
+
+		WebElement oneWay = driver.findElement(By.xpath("//ul[@class='selc-flight-options']/li[1]/a"));
+		// grabn value of any atribute
+		String titleValue = oneWay.getAttribute("title");
+		System.out.println(titleValue);
+
+		//grab cssValue
+		String cssValue = oneWay.getCssValue("color");
+		System.out.println(cssValue);
+
+		//ToolTip - value of title Attribute
+		//Grab tootltip for RoundTrip
+
+		driver.quit();
+
 	}
 
 }

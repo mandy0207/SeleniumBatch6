5c21238aaaebbe0267775ad570e9c3fab2792f4b
unknown <mandyatwork24@gmail.com>
2024-04-15 12:47:38 +0530

    checkboxes

---
+++ src/javaBasics/MethodType.java
@@ -0,0 +1,16 @@
+package javaBasics;
+
+public class MethodType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name=learnType();
+		System.out.println(name);
+	}
+	
+	public static String learnType() {
+		
+		return "vineetha";
+	}
+
+}
---
+++ src/uiComponets/CheckBox_RadioBtn.java
@@ -0,0 +1,34 @@
+package uiComponets;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckBox_RadioBtn {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://selenium.qabible.in/check-box-demo.php");
+		driver.findElement(By.xpath("//*[@id='gridCheck']")).click();
+		
+	
+		List<WebElement> checkBoxes = driver.findElements(By.xpath("//*[@class='check-box-list']"));
+		
+//		for(int i =0 ; i<checkBoxes.size();i++) {
+//			checkBoxes.get(i).click();
+//		}
+		
+		for(WebElement checkbox : checkBoxes) {
+			checkbox.click();
+		}
+		
+		driver.quit();
+		
+
+	}
+
+}
